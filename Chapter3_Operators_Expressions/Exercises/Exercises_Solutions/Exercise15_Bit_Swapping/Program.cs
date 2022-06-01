df0f6110700ecf1ebb42f289479af24594eab203
mrsahin101 <m.rasitsahin@hotmail.com>
2022-06-01 23:06:21 +0300

    Chapter 3 exercises finished

--- Chapter3_Operators_Expressions/Exercises/Exercises_Solutions/Exercise15_Bit_Swapping/Program.cs
+++ Chapter3_Operators_Expressions/Exercises/Exercises_Solutions/Exercise15_Bit_Swapping/Program.cs
@@ -10,21 +10,47 @@ namespace Exercise15_Bit_Swapping
         static void Main(string[] args)
         {
             UInt32 myNumber = rnd32();
+            myNumber = 100;
             Console.WriteLine("Generated random Number :" + myNumber);
+            
+            UInt32 result = Swapper(myNumber, 3, 24);
+            result = Swapper(result, 4, 25);
+            result = Swapper(result, 5, 26);
 
-            //UInt32 temp = 0;
-            //UInt32 tempBit = 0;
-            //temp = myNumber >> 3;
-            //UInt32 junkBit = 1;
-            //tempBit = temp & junkBit;
-
-
+            Console.WriteLine("Result is : " + result);
 
         }
         private static uint rnd32()
         {
             Random rnd = new Random();
             return (uint)(rnd.Next(1 << 30)) << 2 | (uint)(rnd.Next(1 << 2));
+        }
+        private static UInt32 Swapper(UInt32 number, UInt32 indexOne, UInt32 indexTwo)
+        {
+            UInt32 junkBit = 1;
+            UInt32 temporary = 0;
+            UInt32 bitOne = 0;
+            UInt32 bitSecond = 0;
+
+            temporary = number >> (int)indexOne;
+            bitOne = temporary & junkBit;
+            /* temporaryBit equals bit of indexOneTh now*/
+
+            bitSecond = number >> (int)indexTwo;
+            bitSecond = bitSecond & 1;
+
+            if (bitSecond == 0)
+                number = number & ~(junkBit << (int)indexOne);
+            else
+                number = number | (junkBit << (int)indexOne);
+
+
+            if (bitOne == 0)
+                number = number & ~(junkBit << (int)indexTwo);
+            else
+                number = number | (junkBit << (int)indexTwo);
+
+            return number;
         }
     }
 }
---
+++ Chapter3_Operators_Expressions/Exercises/Exercises_Solutions/Exercise16_Bit_Swapper/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+/* Question : Write a program that exchanges bits {p, p+1, …, p+k-1} with bits
+ * {q, q+1, …, q+k-1} of a given 32-bit unsigned integer. 
+ */
+namespace Exercise16_Bit_Swapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UInt32 number;
+            UInt32 arraySize = 0;
+            UInt32[] pArray = new UInt32[32];
+            UInt32[] qArray = new UInt32[32];
+            bool errorFound = false;
+            string pArraystr;
+            string qArraystr;
+            UInt32 result = 0;
+            for (; ; )
+            {
+                Console.Write("Please enter unsigned number : ");
+                string inputNumber = Console.ReadLine();
+                if (UInt32.TryParse(inputNumber, out number))
+                {
+                    Console.Write("Please enter array size : ");
+                    string inputArraySize = Console.ReadLine();
+                    if (UInt32.TryParse(inputArraySize, out arraySize))
+                    {
+                        for(int i = 0; i < arraySize; i++)
+                        {
+                            Console.Write("Enter " + i + "th value for q: ");
+                            pArraystr = (Console.ReadLine());
+                            if (UInt32.TryParse(pArraystr, out pArray[i]) == false)
+                            {
+                                Console.WriteLine("Invalid Input");
+                                errorFound = true;
+                                break;
+                            }
+                                        
+                            Console.Write("Enter " + i + "th value for q: ");
+                            qArraystr = Console.ReadLine();
+                            if (UInt32.TryParse(qArraystr, out qArray[i]) == false)
+                            {
+                                Console.WriteLine("Invalid Input");
+                                errorFound = true;
+                                break;
+                            }
+                        }
+                        if(errorFound == false)
+                        {
+                            result = number;
+
+                            for (int i = 0; i < arraySize;i++)
+                            {
+                                result = Swapper(result,pArray[i],qArray[i]);
+                            }
+                            Console.WriteLine("Calculated Result : " + result + "\n\n");
+                        }
+                    }
+                    else
+                        Console.WriteLine("Invalid input.");
+                }
+                else
+                    Console.WriteLine("Invalid input.");
+
+            }
+        }
+        private static UInt32 Swapper(UInt32 number, UInt32 indexOne, UInt32 indexTwo)
+        {
+            UInt32 junkBit = 1;
+            UInt32 temporary = 0;
+            UInt32 bitOne = 0;
+            UInt32 bitSecond = 0;
+
+            temporary = number >> (int)indexOne;
+            bitOne = temporary & junkBit;
+            /* temporaryBit equals bit of indexOneTh now*/
+
+            bitSecond = number >> (int)indexTwo;
+            bitSecond = bitSecond & 1;
+
+            if (bitSecond == 0)
+                number = number & ~(junkBit << (int)indexOne);
+            else
+                number = number | (junkBit << (int)indexOne);
+
+
+            if (bitOne == 0)
+                number = number & ~(junkBit << (int)indexTwo);
+            else
+                number = number | (junkBit << (int)indexTwo);
+
+            return number;
+        }
+    }
+}
