12a8902a9fbb7755dd844cdc6a3509e57f505d4c
mrsahin101 <m.rasitsahin@hotmail.com>
2022-09-12 23:48:42 +0300

    added few more exercises

--- Chapter7_Arrays/Exercises/Chapter7_Exercises/005_Increasing_Consecutive/Program.cs
+++ Chapter7_Arrays/Exercises/Chapter7_Exercises/005_Increasing_Consecutive/Program.cs
@@ -5,7 +5,7 @@
 int[] myarray = { 3, 2, 3, 4, 2, 2, 4 };
 
 int maximumConsecutive = 1;
-int startingIndex
+int startingIndex = 0;
 int currentConsecutive = 0;
 int previousItem = 0;
 
@@ -16,28 +16,27 @@ for (int i = 0; i < myarray.Length; i++)
         previousItem = myarray[i];
         continue;
     }
-    if (myarray[i] > previousItem)
+    if (myarray[i] ==  previousItem + 1)
     {
         currentConsecutive++;
         if (currentConsecutive > maximumConsecutive)
         {
             maximumConsecutive = currentConsecutive;
-            maximumConsecutiveItem = myarray[i];
+            startingIndex = i - currentConsecutive ;
         }
     }
     else
         currentConsecutive = 0;
     previousItem = myarray[i];
 }
-
 if (maximumConsecutive == 1)
     Console.WriteLine("No consecutive item found! Each item is there only 1 time!");
 else
 {
     Console.Write("{");
-    for (int i = 0; i <= maximumConsecutive; i++)
+    for (int i = startingIndex; i <= startingIndex + maximumConsecutive; i++)
     {
-        Console.Write(maximumConsecutiveItem + ",");
+        Console.Write(myarray[i] + ",");
     }
     Console.Write("}");
 }
---
+++ Chapter7_Arrays/Exercises/Chapter7_Exercises/006_Increasing_Squence/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Write a program, which finds the maximal sequence of increasing elements in an array arr[n]. 
+ * It is not necessary the elements to be consecutively placed. 
+ * E.g.: {9, 6, 2, 7, 4, 7, 6, 5, 8, 4}  {2, 4, 6, 8}.
+ */
+
+int[] myArray = { 9, 6, 2, 7, 4, 7, 6, 5, 8, 4 };
+
+int maxSquenceCount = 1;
+int maxStartingIndex = 0;
+int currentIndexer = 0;
+
+for(int i = 0; i < myArray.Length; i++)
+{
+    int[] temporary = new int[myArray.Length];
+    currentIndexer = 1;
+    temporary[0] = myArray[i];
+    for(int j = i + 1; j < myArray.Length; j++)
+    {
+        if(myArray[j] > temporary[currentIndexer - 1])
+        {
+            temporary[currentIndexer] = myArray[j];
+            currentIndexer++;
+            if(currentIndexer > maxSquenceCount)
+            {
+                maxStartingIndex = i;
+                maxSquenceCount = currentIndexer;
+            }
+        }
+    }
+}
+Console.WriteLine("Starting " + maxStartingIndex + "max count " + maxSquenceCount);
