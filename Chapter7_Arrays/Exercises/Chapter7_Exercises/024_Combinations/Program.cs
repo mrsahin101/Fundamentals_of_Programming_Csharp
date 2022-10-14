aa59082f9a8a272b986756250638e84415585a15
mrsahin101 <m.rasitsahin@hotmail.com>
2022-10-14 23:04:14 +0300

    Chapter 7 done !

--- Chapter7_Arrays/Exercises/Chapter7_Exercises/024_Combinations/Program.cs
+++ Chapter7_Arrays/Exercises/Chapter7_Exercises/024_Combinations/Program.cs
@@ -1,4 +1,54 @@
 ﻿/*  Write a program, which reads an integer number N from the console 
  *  and prints all combinations of K elements of numbers in range [1 … N].
- *  Example: N = 5, K = 2  {1, 2}, {1, 3}, {1, 4}, {1, 5}, {2, 3}, {2, 4}, {2, 5}, {3, 4}, {3, 5}, {4, 5}.
+ *  Example: N = 5, K = 2 -> {1, 2}, {1, 3}, {1, 4}, {1, 5}, {2, 3}, {2, 4}, {2, 5}, {3, 4}, {3, 5}, {4, 5}.
  */
+Console.Write("N :");
+int N = int.Parse(Console.ReadLine());
+
+Console.Write("K :");
+int K = int.Parse(Console.ReadLine());
+
+int[] myValues = new int[K];
+
+for (int i = 0; i < myValues.Length; i++)
+{
+    myValues[i] = 1;
+}
+void printOut()
+{
+    Console.WriteLine();
+    Console.Write("{");
+    for (int i = 0; i < myValues.Length; i++)
+    {
+        Console.Write(myValues[i] + ",");
+    }
+    Console.Write("}");
+}
+int numeric = 0;
+int currentIndexer = 0;
+
+while(true)
+{
+    for (int i = 1; i < N - K + 2; i++)
+    {
+        numeric = i;
+        myValues[0] = numeric++;
+        for (int j = 1; j < K; j++)
+        {
+            myValues[j] = numeric++;
+        }
+        printOut();
+        currentIndexer = K - 1;
+        while(myValues[currentIndexer] != N)
+        {
+            myValues[currentIndexer] += 1;
+            if(myValues[currentIndexer] > N)
+            {
+                myValues[currentIndexer - 1] += 1;
+                myValues[currentIndexer] = 0;
+            }
+            printOut();
+        }
+    }
+    break;
+}
---
+++ Chapter7_Arrays/Exercises/Chapter7_Exercises/025_Find_Longest_Path/Program.cs
@@ -0,0 +1,98 @@
+﻿///* *Write a program, which finds in a given matrix the largest area of equal numbers. 
+// * We define an area in the matrix as a set of neighbor cells (by row and column).
+// * Here is one example with an area containing 13 elements with equal value of 3:
+// * 
+// * 1 *3* 2  2  2  4
+// **3**3**3* 2  4  4
+// * 4 *3* 1  2 *3**3*
+// * 4 *3* 1 *3**3* 1
+// * 4 *3**3**3* 1  1
+// */
+int[,] myArray = new int[5, 6]
+{
+    {1,3,2,2,2,4 },
+    {3,3,3,2,4,4 },
+    {4,3,1,2,3,3 },
+    {4,3,1,3,3,1 },
+    {4,3,3,3,1,1 }
+};
+//int[,] myArray = new int[5, 6]
+//{
+//    {0,0,0,0,0,0 },
+//    {0,0,0,0,0,0 },
+//    {4,1,1,2,3,3 },
+//    {4,1,2,3,3,1 },
+//    {4,3,1,3,1,1 }
+//};
+int ROW = 5;
+int COL = 6;
+bool[,] visited = new bool[5, 6];
+int maxCount = 0;
+for(int i = 0; i < ROW; i++ )
+{
+    for(int j = 0; j < COL; j++)
+    {
+        cleanVisited();
+        int result = checkNeighbour(i, j) + 1;
+        if(result > maxCount)
+        {
+            maxCount = result;
+        }
+    }
+}
+goto end;
+int checkNeighbour(int i, int j)
+{
+    int result = 0;
+    visited[i,j] = true;
+    if(i - 1 >= 0)  //UP
+    {
+        if ((myArray[i,j] == myArray[i - 1,j]) && visited[i - 1,j] == false)
+        {
+            result++;
+            visited[i - 1, j] = true;
+            result += checkNeighbour(i-1,j);
+        }
+    }
+    if(j - 1 >= 0)  //LEFT
+    {
+        if ((myArray[i, j] == myArray[i, j - 1]) && visited[i,j - 1] == false)
+        {
+            result++;
+            visited[i, j - 1] = true;
+            result += checkNeighbour(i, j - 1);
+        }
+    }
+    if(i + 1 <= ROW - 1)    // DOWN
+    {
+        if ((myArray[i, j] == myArray[i + 1, j]) && visited[i + 1, j] == false)
+        {
+            result++;
+            visited[i + 1, j] = true;
+            result += checkNeighbour(i + 1, j);
+        }
+    }
+    if(j + 1 <= COL - 1)    // RIGHT
+    {
+        if ((myArray[i, j] == myArray[i, j + 1]) && visited[i , j + 1] == false)
+        {
+            result++;
+            visited[i , j + 1] = true;
+            result += checkNeighbour(i , j + 1);
+        }
+    }
+    return result ;
+}
+void cleanVisited()
+{
+    for(int i = 0; i < ROW; i++)
+    {
+        for(int j = 0; j < COL; j++)
+        {
+            visited[i,j] = false;
+        }
+    }
+}
+end:
+Console.WriteLine("Calculation Done!");
+Console.WriteLine("Result is : " + maxCount);
