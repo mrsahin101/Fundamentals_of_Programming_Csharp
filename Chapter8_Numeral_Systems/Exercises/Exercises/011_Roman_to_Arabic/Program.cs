f9a2d5e33379f03f6e2059812df03dd2c4ffe1c0
mrsahin101 <m.rasitsahin@hotmail.com>
2022-10-29 23:33:06 +0300

    added some more exercises for chapter8. will finish later

---
+++ Chapter8_Numeral_Systems/Exercises/Exercises/011_Roman_to_Arabic/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Write a program that converts Roman digits to Arabic ones. */
+
+void convert_Roman_to_Arabic(string romanSequence)
+{
+    int sum = 0;
+    Dictionary<string, int> romanMap = new Dictionary<string, int>()
+    {
+        {"I",1 },
+        {"V",5 },
+        {"X",10 },
+        {"L",50 },
+        {"C",100 },
+        {"D",500 },
+        {"M",1000 }
+    };
+    int indexTracker = 0; 
+    for(int i = 0; i < romanSequence.Length; i++)
+    {
+        if (romanMap.ContainsKey(romanSequence[i].ToString()) == true)
+        {
+            if(((i + 1) < romanSequence.Length ) )
+            {
+                if(romanMap.ContainsKey(romanSequence[i + 1].ToString()) == true)
+                {
+                    if (romanSequence[i] < romanSequence[i + 1])
+                        sum -= romanMap[romanSequence[i].ToString()];
+                    else
+                        sum += romanMap[romanSequence[i].ToString()];
+                }
+                else
+                    throw new ArgumentOutOfRangeException("Please use only Roman digits!");
+            }
+            else
+            {
+                sum += romanMap[romanSequence[i].ToString()];
+            }
+        }
+        else
+            throw new ArgumentOutOfRangeException("Please use only Roman digits!");
+        indexTracker++;
+    }
+    Console.WriteLine(sum);
+}
+convert_Roman_to_Arabic("VIM");
---
+++ Chapter8_Numeral_Systems/Exercises/Exercises/014_Data_Types_Experiment/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Try adding up 50,000,000 times the number 0.000001. Use a loop and addition (not direct multiplication). 
+ * Try it with float and double and after that with decimal. 
+ * Do you notice the huge difference in the results and speed of calculation? Explain what happens.
+ */
+using System;
+
+float myFloatSum = 0;
+for(Int32 i = 0; i < 50000000; i++)
+{
+    myFloatSum += 0.000001f;
+}
+Console.WriteLine(myFloatSum);
+
+double myDoubleSum = 0;
+for (Int32 i = 0; i < 50000000; i++)
+{
+    myDoubleSum += 0.000001d;
+}
+Console.WriteLine(myDoubleSum);
+
+decimal myDecimalSum = 0;
+for (Int32 i = 0; i < 50000000; i++)
+{
+    myDecimalSum += 0.000001m;
+}
+Console.WriteLine(myDecimalSum);
